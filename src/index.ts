9866d1609ad02efebcfb94ac5bfd5f6857d1d432
Shane <shane@shanessite.net>
2022-07-24 12:25:57 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { registerPlugin } from '@capacitor/core'
 import type { DarkModePlugin } from './definitions'
 import { kPluginName } from './definitions'
-import * as package from './package.json'
+import * as packageInfo from './package.json'
 import DarkMode from './web'
 
-console.log(`loaded ${package.name}`)
+console.log(`loaded ${packageInfo.name}`)
 
 // Because we are using @aparajita/capacitor-native-decorator,
 // we have one version of the TS code to rule them all, and there
